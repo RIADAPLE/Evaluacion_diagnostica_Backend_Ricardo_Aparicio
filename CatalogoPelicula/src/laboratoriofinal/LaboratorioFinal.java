eb6ed94d14347537d01a8800c3fcf691ef9b29de
riadaple <00032219@uca.edu.sv>
2022-07-30 09:34:08 -0600

    adding try catch to main

--- CatalogoPelicula/src/laboratoriofinal/LaboratorioFinal.java
+++ CatalogoPelicula/src/laboratoriofinal/LaboratorioFinal.java
@@ -12,35 +12,40 @@ public class LaboratorioFinal {
     private static final CatalogoPeliculas catalogoPeliculas = new CatalogoPeliculasImpl();
 
     public static void main(String[] args) {
-        do{
-            Menu();
-            opcion = scanner.nextInt();
-            scanner.nextLine();
-            switch (opcion){
-                case 1:
-                    catalogoPeliculas.iniciarArchivo(nombreArchivo);
-                    break;
-                case 2:
-                    System.out.println("Escriba el nombre de la pelicula que desea insertar");
-                    String nombrePeli = scanner.nextLine();
-                    catalogoPeliculas.agregarPelicula(nombrePeli, nombreArchivo);
-                    break;
-                case 3:
-                    catalogoPeliculas.listarPeliculas(nombreArchivo);
-                    break;
-                case 4:
-                    System.out.println("Escriba el nombre de la pelicula que desea buscar");
-                    String buscarPeli = scanner.nextLine();
-                    catalogoPeliculas.buscarPelicula(buscarPeli, nombreArchivo);
-                    break;
-                case 0:
-                    System.out.println("Se finalizo el programa correctamente");
-                    break;
-                default:
-                    System.out.println("Opcion invalida");
-                    break;
-            }
-        } while (opcion != 0);
+        try{
+            do{
+                Menu();
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+                switch (opcion){
+                    case 1:
+                        catalogoPeliculas.iniciarArchivo(nombreArchivo);
+                        break;
+                    case 2:
+                        System.out.println("Escriba el nombre de la pelicula que desea insertar");
+                        String nombrePeli = scanner.nextLine();
+                        catalogoPeliculas.agregarPelicula(nombrePeli, nombreArchivo);
+                        break;
+                    case 3:
+                        catalogoPeliculas.listarPeliculas(nombreArchivo);
+                        break;
+                    case 4:
+                        System.out.println("Escriba el nombre de la pelicula que desea buscar");
+                        String buscarPeli = scanner.nextLine();
+                        catalogoPeliculas.buscarPelicula(buscarPeli, nombreArchivo);
+                        break;
+                    case 0:
+                        System.out.println("Se finalizo el programa correctamente");
+                        break;
+                    default:
+                        System.out.println("Opcion invalida");
+                        break;
+                }
+            } while (opcion != 0);
+        }
+        catch(Exception e) {
+            System.out.println("!error");
+        }
     }
 
     public static void Menu(){
